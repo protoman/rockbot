9758305877b0b12f0850a2e2b1ca21f936958086
protoman@upperland.net <Rockbot@01>
2015-09-23 21:05:30 -0300

    - improve scenes editor

--- editor/scenes/tab_scenelist.h
+++ editor/scenes/tab_scenelist.h
@@ -2,21 +2,45 @@
 #define SCENESLIST_H
 
 #include <QDialog>
+#include <QStringListModel>
+#include <QStringList>
+#include "scenes/scenesmediator.h"
+#include "file/v3/3_0_1/file_scene.h"
+#include "file/fio_scenes.h"
+#include "model/objectlistmodel.h"
 
 namespace Ui {
 class ScenesList;
 }
 
-class ScenesList : public QDialog
+class TabScenelist : public QDialog
 {
     Q_OBJECT
 
 public:
-    explicit ScenesList(QWidget *parent = 0);
-    ~ScenesList();
+    explicit TabScenelist(QWidget *parent = 0);
+    ~TabScenelist();
+
+
+private:
+    void fill_data();
+    void save_data();
+    void change_fields_enabled(bool value);
+
+private slots:
+    void on_sceneTypeSelector_currentIndexChanged(int index);
+
+    void on_addButton_clicked();
+
+    void on_addScene_button_clicked();
 
 private:
     Ui::ScenesList *ui;
+    QStringListModel model_objects;
+    ObjectListModel model_scenes;
+    ScenesMediator *mediator;
+    bool data_loading;
+    CURRENT_FILE_FORMAT::fio_scenes fio;
 };
 
 #endif // SCENESLIST_H
