fbb734fc16f8b6a6af4b5d77b350af29156870e1
protoman@upperland.net <Rockbot@01>
2015-10-10 13:11:22 -0300

    - start changing file-structs to v4

--- file/fio_strings.h
+++ file/fio_strings.h
@@ -1,10 +1,38 @@
 #ifndef FIO_STRINGS_H
 #define FIO_STRINGS_H
 
+#include <iostream>
+#include <vector>
+#include <cstdlib>
+#include <fstream>
+
+#include "file/v4/file_strings_v4.h"
+
 class fio_strings
 {
 public:
     fio_strings();
 };
+
+
+// namespace
+namespace format_v4 {
+class fio_strings
+{
+public:
+    fio_strings();
+    std::string get_ingame_string(int n);
+
+
+private:
+    std::vector<std::string> load_game_strings();
+    bool file_exists(std::string filename) const;
+    void create_default_ingame_strings();
+
+private:
+    std::vector<std::string> string_list;
+
+};
+} // namespace
 
 #endif // FIO_STRINGS_H
--- strings_map.h
+++ strings_map.h
@@ -1,10 +1,21 @@
 #ifndef STRINGS_MAP_H
 #define STRINGS_MAP_H
 
+#include "defines.h"
+#include "file/fio_strings.h"
+
+#include <iostream>
+
 class strings_map
 {
 public:
     strings_map();
+    static strings_map* get_instance();
+    std::string get_ingame_string(int n);
+
+private:
+    static strings_map* _instance;
+    CURRENT_FILE_FORMAT::fio_strings fio_str;
 };
 
 #endif // STRINGS_MAP_H
