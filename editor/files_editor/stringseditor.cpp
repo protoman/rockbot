94cbd1900cd64338155dba39b9de8c7d7c805cf3
Iuri Fiedoruk <protoman@upperland.net>
2015-10-12 18:29:44 -0300

    - start strings editor screen

--- editor/files_editor/stringseditor.cpp
+++ editor/files_editor/stringseditor.cpp
@@ -1,14 +1,31 @@
 #include "stringseditor.h"
 #include "ui_stringseditor.h"
 
-StringsEditor::StringsEditor(QWidget *parent) :
-    QDialog(parent),
-    ui(new Ui::StringsEditor)
+#include <QLineEdit>
+#include <QSpacerItem>
+
+StringsEditor::StringsEditor(QWidget *parent) : QDialog(parent), ui(new Ui::StringsEditor)
 {
+    std::vector<std::string> string_list = fio_str.load_game_strings();
     ui->setupUi(this);
+
+    connect(signal_mapper, SIGNAL(mapped(const int &)), this, SLOT(onButtonClicked(const int &)));
+
+    for (int i=0; i<string_list.size(); i++) {
+        QLineEdit* line_editor;
+        line_editor = new QLineEdit();
+        line_editor->setText(QString(string_list.at(i).c_str()));
+        ui->tabGame_gridLayout->addWidget(line_editor, i, 0);
+        signal_mapper->setMapping(line_editor, i);
+    }
 }
 
 StringsEditor::~StringsEditor()
 {
     delete ui;
+}
+
+void StringsEditor::on_text_changed(int n, QString value)
+{
+
 }
--- editor/files_editor/stringseditor.h
+++ editor/files_editor/stringseditor.h
@@ -1,7 +1,11 @@
 #ifndef STRINGSEDITOR_H
 #define STRINGSEDITOR_H
 
+#include "file/format.h"
+#include "file/fio_strings.h"
+
 #include <QDialog>
+#include <QSignalMapper>
 
 namespace Ui {
 class StringsEditor;
@@ -15,8 +19,13 @@ public:
     explicit StringsEditor(QWidget *parent = 0);
     ~StringsEditor();
 
+private slots:
+    void on_text_changed(int n, QString value);
+
 private:
     Ui::StringsEditor *ui;
+    CURRENT_FILE_FORMAT::fio_strings fio_str;
+    QSignalMapper signal_mapper;
 };
 
 #endif // STRINGSEDITOR_H
