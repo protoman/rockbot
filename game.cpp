4a9fff4225bf4ca8ce9157c5fd05a33d98265fdd
Iuri Fiedoruk <protoman@upperland.net>
2015-12-03 20:32:28 -0300

    - start beach stage

--- game.cpp
+++ game.cpp
@@ -1068,7 +1068,7 @@ void game::quick_load_game()
     if (fio.save_exists()) {
         fio.read_save(game_save);
     }
-    currentStage = STAGE2;
+    currentStage = STAGE4;
     game_save.selected_player = PLAYER_ROCKBOT;
     if (GAME_FLAGS[FLAG_PLAYER_ROCKBOT]) {
         game_save.selected_player = PLAYER_ROCKBOT;
