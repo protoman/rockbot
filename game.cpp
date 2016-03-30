97ffc469656d402b15cf818e4dad67d1f27155f3
Iuri Fiedoruk <protoman@upperland.net>
2016-03-29 22:56:54 -0300

    - edit phantom stage

--- game.cpp
+++ game.cpp
@@ -1164,7 +1164,7 @@ void game::quick_load_game()
     if (fio.save_exists()) {
         fio.read_save(game_save);
     }
-    currentStage = CASTLE1_STAGE1;
+    currentStage = STAGE7;
     game_save.difficulty = DIFFICULTY_EASY;
     game_save.selected_player = PLAYER_1;
 
