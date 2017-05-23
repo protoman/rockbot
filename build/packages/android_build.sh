c734fa6ea7e4be5130fbaa312c8541ad87a75c6b
Iuri Fiedoruk <protoman@upperland.net>
2017-05-22 22:19:53 -0300

    - update roadmap

--- build/packages/android_build.sh
+++ build/packages/android_build.sh
@@ -1,20 +1,24 @@
-#!/bin/sh
+#!/bin/bash
 
 #set -x
 
 read -r -p "What is the version number you are building? " version_number
-if ([ "$version_number" != "1" ] && [ "$version_number" != "2" ]) then
+if [ "$version_number" != "1" ] && [ "$version_number" != "2" ];
+then
     echo "Invalid version number"
-    return
+    exit
 fi
 
-demoName = ""
-demoFullName = ""
-if ["$version_number" == "2"] then
+demoName=""
+demoFullName=""
+
+if [ "$version_number" == "2" ]
+then
     read -r -p "Build demo version (y) or full(n)? " demo_mode
-    if [ "demo_mode" == "y" ] then
-        demoName = "Demo"
-        demoFullName = "demo"
+    if [ "$demo_mode" == "y" ]
+    then
+        demoName="Demo"
+        demoFullName="demo"
     fi
 fi
 
@@ -39,10 +43,10 @@ case $response in
 		rm ./Android/data/game*.sav
 		rm ./Android/data/config_v*.sav
 		cd ./Android/data
-		zip -r ../data_$VERSIONNAME.zip ./fonts ./games ./shared
+		zip -r ../data_$VERSIONNAME$demoName.zip ./fonts ./games ./shared
 		cd ..
 		rm $ANDROIDSDK/rockbot_build/project/jni/application/src/AndroidData/*.zip
-		cp ./data_$VERSIONNAME.zip $ANDROIDSDK/rockbot_build/project/jni/application/src/AndroidData/
+		cp ./data_$VERSIONNAME$demoName.zip $ANDROIDSDK/rockbot_build/project/jni/application/src/AndroidData/
 		rm $ANDROIDSDK/rockbot_build/project/jni/application/src/libapplication.so
 		rm $ANDROIDSDK/rockbot_build/project/obj/local/armeabi/libapplication.so
 		rm $ANDROIDSDK/rockbot_build/project/libs/armeabi/libapplication.so
@@ -80,7 +84,7 @@ case $response in
                     return
 		fi
 		
-		sed $LINENUMBER'c\'"AppDataDownloadUrl=\"!Game Data|data_$VERSIONNAME.zip\"" AndroidAppSettings.cfg > AndroidAppSettings.cfg.temp1
+		sed $LINENUMBER'c\'"AppDataDownloadUrl=\"!Game Data|data_$VERSIONNAME$demoName.zip\"" AndroidAppSettings.cfg > AndroidAppSettings.cfg.temp1
 		sed $LINENUMBERVERSION'c\'"AppVersionName=\"$VERSIONNAME\"" AndroidAppSettings.cfg.temp1 > AndroidAppSettings.cfg.temp2
 		sed $LINENUMBERAPPNAME'c\'"AppName=\"Rockbot$version_number$demoName\"" AndroidAppSettings.cfg.temp2 > AndroidAppSettings.cfg.temp3
 		if [ "$version_number" == "1" ]; then
@@ -100,15 +104,15 @@ case $response in
 		
 		# build release
 		sh ./build.sh rockbot release
-		rm $ROCKBOTDIR/Rockbot_Android_$VERSIONNAME.apk
-		cp ./project/bin/MainActivity-release-unsigned.apk $ROCKBOTDIR/TEMP_Rockbot_Android_$VERSIONNAME.apk
+		rm $ROCKBOTDIR/Rockbot_Android_$VERSIONNAME$demoName.apk
+		cp ./project/bin/MainActivity-release-unsigned.apk $ROCKBOTDIR/TEMP_Rockbot_Android_$VERSIONNAME$demoName.apk
 		# remove as assinaturas do Android (caso haja alguma, por engano)
-		zip -d $ROCKBOTDIR/TEMP_Rockbot_Android_$VERSIONNAME.apk META-INF/*
+		zip -d $ROCKBOTDIR/TEMP_Rockbot_Android_$VERSIONNAME$demoName.apk META-INF/*
 		# assina e realinha o APK
-		/opt/java/jdk1.8.0_121/bin/jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore ~/.android/my-release-key.keystore  $ROCKBOTDIR/TEMP_Rockbot_Android_$VERSIONNAME.apk alias_name
-		/home/iuri/Programas/android-studio/sdk/build-tools/23.0.3/zipalign -v 4 $ROCKBOTDIR/TEMP_Rockbot_Android_$VERSIONNAME.apk $ROCKBOTDIR/Rockbot_Android_$VERSIONNAME.apk
+		/opt/java/jdk1.8.0_121/bin/jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore ~/.android/my-release-key.keystore  $ROCKBOTDIR/TEMP_Rockbot_Android_$VERSIONNAME$demoName.apk alias_name
+		/home/iuri/Programas/android-studio/sdk/build-tools/23.0.3/zipalign -v 4 $ROCKBOTDIR/TEMP_Rockbot_Android_$VERSIONNAME$demoName.apk $ROCKBOTDIR/Rockbot_Android_$VERSIONNAME$demoName.apk
 		# copy mappings.txt so we can use for later debugging
-		cp ./project/bin/proguard/mapping.txt $ROCKBOTDIR/Tmappings_$VERSIONNAME.txt
+		cp ./project/bin/proguard/mapping.txt $ROCKBOTDIR/Tmappings_$VERSIONNAME$demoName.txt
 		;;
 	*)
 		echo "Aborted."
--- build/packages/win32.sh
+++ build/packages/win32.sh
@@ -10,14 +10,17 @@ fi
 VERSIONNAME=`cat version_name_v$version_number.txt`
 
 #copy data
+rm -r -f ./win32/rockbot
 mkdir -p ./win32/rockbot/games
 rsync -r --exclude=.svn ../fonts ./win32/rockbot
 rsync -r --exclude=.svn ../shared ./win32/rockbot
-rsync -r --exclude=.svn ../games/Rockbot$version_number ./win32/rockbot
+rsync -r --exclude=.svn ../games/Rockbot$version_number ./win32/rockbot/games
+cp ./win32/*.dll ./win32/rockbot
+cp ./win32/*.exe ./win32/rockbot
 rm ./win32/rockbot/game*.sav
 rm ./win32/rockbot/config_v*.sav
 
 
-cd win32
+cd win32/rockbot
 spacer="_WIN32_"
-zip -r ../Rockbot$version_number$spacer$VERSIONNAME.zip *
+zip -r ../../Rockbot$version_number$spacer$VERSIONNAME.zip *
