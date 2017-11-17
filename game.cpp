df405ec6914b87d5a8d215892d166a315aa5b241
Iuri Fiedoruk <protoman@upperland.net>
2017-11-17 16:29:06 -0200

    - partial for #69

--- game.cpp
+++ game.cpp
@@ -1423,7 +1423,7 @@ void game::quick_load_game()
         fio.read_save(game_save, current_save_slot);
     }
 
-    currentStage = CASTLE1_STAGE5;
+    currentStage = CASTLE1_STAGE1;
     game_save.difficulty = DIFFICULTY_HARD;
     game_save.selected_player = PLAYER_3;
 
@@ -1448,7 +1448,7 @@ void game::quick_load_game()
     scenes.preloadScenes();
 
     // TEST //
-    currentStage = scenes.pick_stage(INTRO_STAGE);
+    //currentStage = scenes.pick_stage(INTRO_STAGE);
 
     // DEBUG //
     std::cout << "############### currentStage[" << (int)currentStage << "]" << std::endl;
