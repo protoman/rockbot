8da1979f29fa2745702beed7cbaff4fad3ae81c9
Iuri Fiedoruk <protoman@upperland.net>
2018-01-03 14:21:38 -0300

    - remove debug

--- file/file_io.cpp
+++ file/file_io.cpp
@@ -895,6 +895,7 @@ namespace format_v4 {
 
         // ------- DEBUG ------- //
 
+        /*
         data_out.stages[INTRO_STAGE] = 1;
         for (int i=STAGE1; i<=STAGE8; i++) {
             data_out.stages[i] = 1;
@@ -903,7 +904,6 @@ namespace format_v4 {
         for (int i=CASTLE1_STAGE1; i<=CASTLE1_STAGE5; i++) {
             data_out.stages[i] = 1;
         }
-        /*
         */
         //data_out.stages[INTRO_STAGE] = 1;
         //data_out.stages[STAGE5] = 1;
