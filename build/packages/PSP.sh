3e90bc46c381c3f1e7f2e677c3258d359840bdb9
Iuri Fiedoruk <protoman@upperland.net>
2023-10-03 22:22:14 -0300

    -update script

--- build/packages/PSP.sh
+++ build/packages/PSP.sh
@@ -6,16 +6,25 @@ then
     exit
 fi
 
+set -x
+
 VERSIONNAME=`cat version_name_v$version_number.txt`
 GAME_DIR=Rockbot$version_number
 
-cd ..
-make clean
-cd ..
-qmake ../RockDroid.pro CONFIG -=linux CONFIG+=psp
-cd build
-make
-cd packages
+rm -r -f ./PSP/$GAME_DIR
+
+#cd ..
+#make clean
+#cd ..
+#qmake ../RockDroid.pro CONFIG -=linux CONFIG+=psp
+#cd build
+#make
+#cd packages
+
+cd ../..
+pwd
+make -f Makefile.psp clean
+make -f Makefile.psp version_number=$version_number VERSIONNAME=$VERSIONNAME
 
 rm -r -f ./PSP
 mkdir ./PSP
@@ -23,10 +32,10 @@ mkdir ./PSP/$GAME_DIR
 mkdir ./PSP/$GAME_DIR/games
 mkdir ./PSP/$GAME_DIR/fonts
 mkdir ./PSP/$GAME_DIR/shared
-cp ../EBOOT.PBP ./PSP/$GAME_DIR/
-rsync -r --exclude=mp3 --exclude=.svn ../games/RockDroid$version_number ./PSP/$GAME_DIR/games
-rsync -r --exclude=.svn ../fonts ./PSP/$GAME_DIR
-rsync -r --exclude=.svn ../shared ./PSP/$GAME_DIR
+cp ./EBOOT.PBP ./PSP/$GAME_DIR/
+rsync -r --exclude=mp3 --exclude=.svn ./build/games/RockDroid$version_number ./PSP/$GAME_DIR/games
+rsync -r --exclude=.svn ./build/fonts ./PSP/$GAME_DIR
+rsync -r --exclude=.svn ./build/shared ./PSP/$GAME_DIR
 rm ./PSP/$GAME_DIR/data/game*.sav
 rm ./PSP/$GAME_DIR/data/config*.sav
 cd ./PSP
